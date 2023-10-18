6d52be1cecfc6011b19429ffa42ac5200d5ff1a8
Fedhori <edhori990611@gmail.com>
2023-10-18 09:51:51 +0000

    remove the implementation of sum_of_square function

--- main.py
+++ main.py
@@ -12,19 +12,15 @@ def even_list(int_list: List[int]) -> List[int]:
 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
 
-    sum = 0
-    for value in even_int_list:
-        if value % 2 == 0:
-            sum += value * value
+    # TODO: Implement sum_of_squares_of_even
 
-    return sum
+    pass
 
 # Main function
 def main():
     # Example list
     int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     even_int_list = even_list(int_list)
-    print(even_int_list)
     output = sum_of_squares_of_even(even_int_list)
     print(output)
 
