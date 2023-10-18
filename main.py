a4b75a4d0c6d41851bc498012cf2887440d39733
Fedhori <edhori990611@gmail.com>
2023-10-18 10:07:41 +0000

    implement sum_of_square function

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import List
+
+def even_list(int_list: List[int]) -> List[int]:
+
+    even_int_list = []
+
+    for value in int_list:
+        if value % 2 == 0:
+            even_int_list.append(value)
+
+    return even_int_list
+
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    print(even_int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+    pass
+
+# Main function
+def main():
+    # Example list
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+# Boilerplate code
+
+if __name__ == "__main__":
+    main()
